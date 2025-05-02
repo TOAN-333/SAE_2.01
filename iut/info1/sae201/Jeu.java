bc81a4ad1a367c37192c128570a74a412c58ffe1
Hery Toan <toanhery333@gmail.com>
2025-05-02 09:45:57 +0200

    Création de la classe Jeu (test)

---
+++ iut/info1/sae201/Jeu.java
@@ -0,0 +1,23 @@
+/**
+ * Date.java                     30/04/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
+package iut.info1.sae201;
+
+/**
+ * Date du calendrier grégorien allant de 1850 à 2050
+ * 
+ * @author Belahsen Sofia
+ * @author Delperie Lola
+ * @author Dumas Enzo
+ * @author Hery Toan
+ * @author Pichon Raphael
+ */
+public class Jeu {
+	// Empty body
+	
+	//V2
+	String nom;
+	
+}
