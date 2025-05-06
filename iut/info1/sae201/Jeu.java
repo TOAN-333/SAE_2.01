ba9c5c009857b0e0323081dfd00402ba1f534cc4
Dumas Enzo <enzo.dumas@iut-rodez.fr>
2025-05-06 13:38:56 +0200

    Ajout de la V0 (a completer) du fichier Jeu.java)

--- iut/info1/sae201/Jeu.java
+++ iut/info1/sae201/Jeu.java
@@ -6,17 +6,85 @@
 package iut.info1.sae201;
 
 /**
- * Date du calendrier grégorien allant de 1850 à 2050
  * 
+ * Cette classe permet de créer des instances "pion" construits
+ * selon les caractéristiques précisées en tant qu'argument 
+ * du constructeur.
  * @author Bourgougnon Thomas
  * @author Dalle Nathael
  * @author Dumas Enzo
  * @author Hery Toan
  */
 public class Jeu {
-	// Empty body
-	//modification de commentaire Toan
-	// V4
-	String nom;
-	
+    
+    private static final int LIGNES = 7;
+    
+    private static final int COLONNES = 6;
+    
+    private int coX;
+    private int coY;
+    private int jaune;
+    private int rouge;
+    String couleur;
+    
+    int[][]plateau = {
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0}
+    };
+
+    /**
+     * TODO commenter l'etat initial du constructeur
+     * @param coX
+     * @param coY
+     * @param couleur
+     */
+    public Jeu(int coX, int coY, String couleur) {
+        super();
+        this.coX = coX;
+        this.coY = coY;
+        this.couleur = couleur;
+    }
+    
+    /**
+     * TODO commenter la fonctionnalité de la méthode
+     */
+    public void bvureyvb() {
+        while(rouge < 4 || jaune < 4) {
+            //empty body
+        }
+    }
+    
+    /**
+     * TODO commenter la fonctionnalité de la méthode
+     * @param coX
+     * @param coY
+     * @return true si un des deux joueurs gagne
+     */
+    public boolean verifierVictoire(int coX, int coY) {
+        //char symbole = plateau[coX][coY];
+        return false; //TODO faire le returngit 
+        
+    }
+    
+    
+    /**
+     * TODO commenter la fonctionnalité de la méthode
+     * @return 
+     */
+    private int compterALignes(int coX, int coY, int dCoX, int dCoY, char symbole) {
+        int count;
+        int l = coX + dCoX;
+        int c = coY + dCoY;
+        
+        for(count = 0; l >= 0 && 1 < LIGNES && c>= 0 && c < COLONNES 
+                && plateau[l][c] == symbole; count++) {
+            l += dCoX;
+            c += dCoY;
+        }
+        return count;
+    }
 }
