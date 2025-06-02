ebfbc17f98925feafd69489fdc0d070a11602e28
Toan Hery <toan.hery@iut-rodez.fr>
2025-06-02 09:00:40 +0200

    ajout des commentaires

--- iut/info1/sae201/vue/EchangeurDeVue.java
+++ iut/info1/sae201/vue/EchangeurDeVue.java
@@ -1,3 +1,8 @@
+/**
+ * EchangeurDeVue.java                       02/06/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
 package iut.info1.sae201.vue;
 
 import java.io.IOException;
@@ -8,30 +13,79 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+/**
+ * La classe {@code EchangeurDeVue} gère le changement de vue au sein de 
+ * l'application JavaFX.
+ * 
+ * Elle permet de charger des fichiers FXML pour améliorer les performances 
+ * lors des changements de scène.
+ * 
+ * <p>
+ * 		Cette classe utilise {@link EnsembleDesVues} pour déterminer le 
+ * 		nom du fichier FXML à charger à partir d'un code de vue donné.
+ * </p>
+ * 
+ * <p>
+ * 		Elle requiert qu'une {@link Scene} courante soit définie avant 
+ * 		tout échange de vue.
+ * </p>
+ * 
+ * @author Thomas Bourgougnon
+ * @author Nathael Dalle
+ * @author Enzo Dumas
+ * @author Toan Hery
+ */
 public class EchangeurDeVue {
 
+    /** Cache des vues déjà chargées, associées à leur code vue. */
     private static Map<Integer, Parent> cache = new HashMap<>();
+
+    /** Scène JavaFX actuellement affichée. */
     private static Scene sceneCourante;
 
+    /**
+     * Définit la scène courante de l'application.
+     *
+     * @param nouvelleScene la scène à utiliser pour les futurs échanges de vue
+     */
     public static void setSceneCourante(Scene nouvelleScene) {
         sceneCourante = nouvelleScene;
     }
 
+    /**
+     * Remplace le contenu de la scène courante par celui correspondant 
+     * au code de vue donné.
+     * 
+     * Si la vue a déjà été chargée, elle est récupérée. 
+     * 
+     * Sinon elle est chargée depuis le fichier FXML correspondant 
+     * et ajoutée au cache.
+     *
+     * @param codeVue le code de la vue à afficher
+     * @throws IllegalStateException si la scène courante n'a pas été définie
+     */
     public static void echangerAvec(int codeVue) {
         if (sceneCourante == null) {
-            throw new IllegalStateException("Echange de vue impossible. Pas de scène courante.");
+            throw new IllegalStateException("Échange de vue impossible. Pas de scène courante.");
         }
+
         try {
             Parent racine;
+
+            // Vérifie si la vue est déjà en cache
             if (cache.containsKey(codeVue)) {
                 racine = cache.get(codeVue);
             } else {
-                racine = FXMLLoader.load(EchangeurDeVue.class.getResource(EnsembleDesVues.getNomVue(codeVue)));
+                racine = FXMLLoader.load(EchangeurDeVue.class.getResource(
+                        EnsembleDesVues.getNomVue(codeVue)));
                 cache.put(codeVue, racine);
             }
+
+            // Met à jour la racine de la scène
             sceneCourante.setRoot(racine);
+
         } catch (IOException e) {
-            System.out.println("Echec du chargement de la vue de code " + codeVue);
+            System.out.println("Échec du chargement de la vue de code " + codeVue);
             e.printStackTrace();
         }
     }
--- iut/info1/sae201/vue/EnsembleDesVues.java
+++ iut/info1/sae201/vue/EnsembleDesVues.java
@@ -1,19 +1,53 @@
+/**
+ * EnsembleDesVues.java                       02/06/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
+
 package iut.info1.sae201.vue;
 
+/**
+ * La classe {@code EnsembleDesVues} centralise les différentes vues de 
+ * l'application. Chaque vue est associée à un identifiant entier constant
+ * utilisé pour référencer un fichier FXML correspondant.
+ * 
+ * <p>
+ *     Cette classe permet de récupérer le nom du fichier FXML 
+ *     associé à un code vue donné.
+ * </p>
+ * 
+ * @author Thomas Bourgougnon
+ * @author Nathael Dalle
+ * @author Enzo Dumas
+ * @author Toan Hery
+ */
 public class EnsembleDesVues {
+
+    /** Code représentant la vue du menu principal. */
     public static final int VUE_MENU = 0;
+
+    /** Code représentant la vue du jeu Puissance 4. */
     public static final int VUE_PUISSANCE4 = 1;
+
+    /** Code représentant la vue des paramètres. */
     public static final int VUE_PARAMETRE = 2;
 
+    /** Tableau contenant les noms des fichiers FXML correspondant aux vues. */
     private static final String[] NOM_DES_VUES = {
         "Menu.fxml", "Puissance4.fxml", "Parametre.fxml"
     };
 
+    /**
+     * Retourne le nom du fichier FXML associé au code de la vue fourni.
+     *
+     * @param codeVue le code de la vue (doit être compris entre 0 et {@code NOM_DES_VUES.length - 1})
+     * @return le nom du fichier FXML correspondant à la vue
+     * @throws IllegalArgumentException si le code de la vue est invalide
+     */
     public static String getNomVue(int codeVue) {
         if (codeVue < 0 || codeVue >= NOM_DES_VUES.length) {
             throw new IllegalArgumentException("Code vue " + codeVue + " invalide");
         }
         return NOM_DES_VUES[codeVue];
     }
-    
 }
