bf3dd9c57455a88c2bfeae4af375a1bfde040ca4
Toan Hery <toan.hery@iut-rodez.fr>
2025-06-02 09:11:53 +0200

    Ajout des commentaires

--- iut/info1/sae201/modele/Jeu.java
+++ iut/info1/sae201/modele/Jeu.java
@@ -1,44 +1,83 @@
+/**
+ * Jeu.java                                  02/06/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
 package iut.info1.sae201.modele;
 
+/**
+ * La classe {@code Jeu} modélise le fonctionnement du jeu Puissance 4.
+ * Elle gère la grille de jeu, le placement des jetons, la détection 
+ * de victoire, ainsi que les paramètres liés aux joueurs et à la couleur 
+ * de la grille.
+ * 
+ * <p>
+ * 		Elle prend en charge une grille de 6 lignes par 7 colonnes.
+ * </p>
+ * 
+ * @author Thomas Bourgougnon
+ * @author Nathael Dalle
+ * @author Enzo Dumas
+ * @author Toan Hery
+ */
 public class Jeu {
-    
+
+    /** Nombre de lignes de la grille. */
     public static final int LIGNES = 6;
+
+    /** Nombre de colonnes de la grille. */
     public static final int COLONNES = 7;
-    
+
+    /** Grille de jeu représentée par une matrice de chaînes. */
     private String[][] grille;
-    private boolean rougeJoue; 
+
+    /** Indique si c'est au joueur rouge de jouer. */
+    private boolean rougeJoue;
+
+    /** Indique si la partie est terminée. */
     private boolean partieTerminee;
 
-    // Nouveaux attributs pour stocker les paramètres joueurs
+    // Paramètres des joueurs
     private String pseudoJoueur1;
     private String pseudoJoueur2;
+    
     private String couleurJoueur1;
     private String couleurJoueur2;
+    
     private String couleurGrille;
 
-    // Constructeur par défaut
+    /**
+     * Initialise une nouvelle grille vide et prépare la partie.
+     */
     public Jeu() {
         initialiserGrille();
     }
-    
-    // Nouveau constructeur avec paramètres
+
+    /**
+     *
+     * @param parametres les paramètres de la partie (pseudos, couleurs, etc.)
+     */
     public Jeu(Parametres parametres) {
         this.pseudoJoueur1 = parametres.getPseudoJoueur1();
         this.pseudoJoueur2 = parametres.getPseudoJoueur2();
         this.couleurJoueur1 = parametres.getCouleurJoueur1();
         this.couleurJoueur2 = parametres.getCouleurJoueur2();
         this.couleurGrille = parametres.getCouleurGrille();
+
         initialiserGrille();
-        rougeJoue = true; // Joueur 1 commence toujours
+        rougeJoue = true;
         partieTerminee = false;
-        
-        // Optionnel : afficher dans la console ou logger
+
+        // Debug console (peut être remplacé par un Logger)
         System.out.println("Partie démarrée avec :");
         System.out.println("Joueur 1: " + pseudoJoueur1 + " en " + couleurJoueur1);
         System.out.println("Joueur 2: " + pseudoJoueur2 + " en " + couleurJoueur2);
         System.out.println("Couleur grille: " + couleurGrille);
     }
 
+    /**
+     * Initialise la grille de jeu avec des cellules vides.
+     */
     public void initialiserGrille() {
         grille = new String[LIGNES][COLONNES];
         for (int i = 0; i < LIGNES; i++) {
@@ -50,6 +89,12 @@ public class Jeu {
         rougeJoue = true;
     }
 
+    /**
+     * Tente de placer un jeton dans la colonne indiquée.
+     *
+     * @param colonne numéro de la colonne (1 à 7)
+     * @return l'indice de ligne où le jeton a été placé, ou -1 si la colonne est pleine
+     */
     public int placerJeton(int colonne) {
         int colIndex = colonne - 1;
         for (int i = LIGNES - 1; i >= 0; i--) {
@@ -61,14 +106,34 @@ public class Jeu {
         return -1;
     }
 
+    /**
+     * Vérifie s'il y a une victoire depuis la position spécifiée.
+     *
+     * @param ligne ligne du dernier jeton placé
+     * @param colonne colonne du dernier jeton placé
+     * @return {@code true} si un alignement gagnant est détecté, sinon {@code false}
+     */
     public boolean verifierVictoire(int ligne, int colonne) {
         String symbole = grille[ligne][colonne];
-        return (compterAlignes(ligne, colonne, 0, 1, symbole) + compterAlignes(ligne, colonne, 0, -1, symbole) >= 3 ||
-               (compterAlignes(ligne, colonne, 1, 0, symbole) >= 3) ||
-               (compterAlignes(ligne, colonne, 1, 1, symbole) + compterAlignes(ligne, colonne, -1, -1, symbole) >= 3) ||
-               (compterAlignes(ligne, colonne, 1, -1, symbole) + compterAlignes(ligne, colonne, -1, 1, symbole) >= 3));
+        return (compterAlignes(ligne, colonne, 0, 1, symbole) + 
+                compterAlignes(ligne, colonne, 0, -1, symbole) >= 3 ||
+                compterAlignes(ligne, colonne, 1, 0, symbole) >= 3 ||
+                compterAlignes(ligne, colonne, 1, 1, symbole) + 
+                compterAlignes(ligne, colonne, -1, -1, symbole) >= 3 ||
+                compterAlignes(ligne, colonne, 1, -1, symbole) + 
+                compterAlignes(ligne, colonne, -1, 1, symbole) >= 3);
     }
 
+    /**
+     * Compte le nombre de jetons alignés dans une direction donnée.
+     *
+     * @param ligne ligne de départ
+     * @param colonne colonne de départ
+     * @param deltaLigne direction verticale
+     * @param deltaColonne direction horizontale
+     * @param symbole symbole à compter ("R" ou "J")
+     * @return le nombre de jetons alignés consécutifs dans la direction donnée
+     */
     private int compterAlignes(int ligne, int colonne, int deltaLigne, int deltaColonne, String symbole) {
         int count = 0;
         int l = ligne + deltaLigne;
@@ -81,6 +146,11 @@ public class Jeu {
         return count;
     }
 
+    /**
+     * Vérifie si la grille est complètement remplie.
+     *
+     * @return {@code true} si la grille est pleine, sinon {@code false}
+     */
     public boolean estGrillePleine() {
         for (int j = 0; j < COLONNES; j++) {
             if (grille[0][j].equals(".")) return false;
@@ -88,17 +158,47 @@ public class Jeu {
         return true;
     }
 
-    // Getters pour les nouveaux attributs si besoin
-    public String getPseudoJoueur1() { return pseudoJoueur1; }
-    public String getPseudoJoueur2() { return pseudoJoueur2; }
-    public String getCouleurJoueur1() { return couleurJoueur1; }
-    public String getCouleurJoueur2() { return couleurJoueur2; }
-    public String getCouleurGrille() { return couleurGrille; }
+    // Getters pour les paramètres joueurs
 
-    // Getters et setters existants
-    public String[][] getGrille() { return grille; }
-    public boolean isRougeJoue() { return rougeJoue; }
-    public void setRougeJoue(boolean rougeJoue) { this.rougeJoue = rougeJoue; }
-    public boolean isPartieTerminee() { return partieTerminee; }
-    public void setPartieTerminee(boolean partieTerminee) { this.partieTerminee = partieTerminee; }
+    public String getPseudoJoueur1() { 
+    	return pseudoJoueur1; 
+    }
+
+    public String getPseudoJoueur2() { 
+    	return pseudoJoueur2; 
+    }
+
+    public String getCouleurJoueur1() { 
+    	return couleurJoueur1; 
+    }
+
+    public String getCouleurJoueur2() { 
+    	return couleurJoueur2; 
+    }
+
+    public String getCouleurGrille() { 
+    	return couleurGrille; 
+    }
+
+    // Getters et setters
+
+    public String[][] getGrille() { 
+    	return grille; 
+    }
+
+    public boolean isRougeJoue() { 
+    	return rougeJoue; 
+    }
+
+    public void setRougeJoue(boolean rougeJoue) { 
+    	this.rougeJoue = rougeJoue; 
+    }
+
+    public boolean isPartieTerminee() { 
+    	return partieTerminee; 
+    }
+
+    public void setPartieTerminee(boolean partieTerminee) { 
+    	this.partieTerminee = partieTerminee; 
+    }
 }
