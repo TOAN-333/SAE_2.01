05b8ddfa0cb9e313c7c09d875b84df3fa9df4ad5
Enzo Dumas <enzo.dumas@iut-rodez.fr>
2025-06-06 16:30:40 +0200

    Ajout des fichiers fxml et des classes gérant la vue de l'application

--- iut/info1/sae201/vue/EchangeurDeVue.java
+++ iut/info1/sae201/vue/EchangeurDeVue.java
@@ -1,3 +1,8 @@
+/**
+ * CEchangeurDeVue.java                10/05/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
 package iut.info1.sae201.vue;
 
 import java.io.IOException;
@@ -5,24 +10,52 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+/**
+ * Cette classe permet de gérer le changement de vue (écran) 
+ * dans une application JavaFX.
+ * 
+ * @author Toan Hery
+ * @author Enzo Dumas
+ * @author Nathael Dalle
+ * @author Thomas Bourgougnon
+ */
 public class EchangeurDeVue {
 
+    /**
+     * La scène actuellement affichée dans l'application.
+     */
     private static Scene sceneCourante;
 
+    /**
+     * Définit la scène courante sur laquelle les vues vont être échangées.
+     * 
+     * @param nouvelleScene la nouvelle scène à utiliser comme scène courante
+     */
     public static void setSceneCourante(Scene nouvelleScene) {
         sceneCourante = nouvelleScene;
     }
 
+    /**
+     * Change la vue affichée dans la scène courante en fonction du code de la vue.
+     * 
+     * @param codeVue le code correspondant à la vue à afficher
+     * @throws IllegalStateException si la scène courante n'est pas encore définie
+     */
     public static void echangerAvec(int codeVue) {
+        // Vérifie que la scène courante est définie avant de changer la vue
         if (sceneCourante == null) {
             throw new IllegalStateException("Échange de vue impossible. Aucune scène courante n'est définie.");
         }
 
         try {
+            // Charge le fichier FXML correspondant à la vue donnée
             FXMLLoader loader = new FXMLLoader(EchangeurDeVue.class.getResource(EnsembleDesVues.getNomVue(codeVue)));
             Parent racine = loader.load();
+
+            // Remplace la racine de la scène courante par la nouvelle vue chargée
             sceneCourante.setRoot(racine);
         } catch (IOException e) {
+            // Affiche un message d'erreur si le chargement du fichier FXML échoue
             System.err.println("Échec du chargement de la vue avec le code " + codeVue);
             e.printStackTrace();
         }
--- iut/info1/sae201/vue/EnsembleDesVues.java
+++ iut/info1/sae201/vue/EnsembleDesVues.java
@@ -1,13 +1,41 @@
+/**
+ * EnsembleDesVues.java                10/05/2025
+ * 
+ * IUT de Rodez, 2024-2025, aucun copyright
+ */
 package iut.info1.sae201.vue;
 
+/**
+ * Cette classe contient la liste des vues disponibles dans l'application,
+ * identifiées par des codes entiers, ainsi que les noms des fichiers FXML associés.
+ * 
+ * Elle fournit aussi une méthode pour récupérer le nom d'une vue à partir de son code.
+ * 
+ * @author Toan Hery
+ * @author Enzo Dumas
+ * @author Nathael Dalle
+ * @author Thomas Bourgougnon
+ */
 public class EnsembleDesVues {
+
+    /** Code pour la vue du menu principal */
     public static final int VUE_MENU = 0;
+
+    /** Code pour la vue du jeu Puissance4 */
     public static final int VUE_PUISSANCE4 = 1;
 
+    /** Tableau contenant les noms des fichiers FXML des vues */
     private static final String[] NOM_DES_VUES = {
         "Menu.fxml", "puissance4.fxml"
     };
 
+    /**
+     * Retourne le nom du fichier FXML correspondant au code de la vue donné.
+     * 
+     * @param codeVue le code entier de la vue
+     * @return le nom du fichier FXML associé à cette vue
+     * @throws IllegalArgumentException si le code de la vue est invalide (hors limite)
+     */
     public static String getNomVue(int codeVue) {
         if (codeVue < 0 || codeVue >= NOM_DES_VUES.length) {
             throw new IllegalArgumentException("Code vue " + codeVue + " invalide");
